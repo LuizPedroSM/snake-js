032f2b969f3d18629f01418ba2d9faddce161ca2
Luiz Pedro <luizpsmoreira@gmail.com>
2020-03-20 20:29:38 -0300

    add Food

--- script.js
+++ script.js
@@ -9,17 +9,26 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
-function makeBackground() {
+function drawBackground() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box, 16 * box);
 }
 
-function makeSnake() {
+function drawSnake() {
     for (let i = 0; i < snake.length; i++) {
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener("keydown", update);
@@ -39,8 +48,9 @@ function startGame() {
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;
 
-    makeBackground();
-    makeSnake();
+    drawBackground();
+    drawSnake();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
